72334e0718202d82012c1437564b4a8765544659
Ilya Zarudny <zaruda46@gmail.com>
2021-07-13 17:31:18 +0300

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import Head from 'next/head';
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../theme'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
